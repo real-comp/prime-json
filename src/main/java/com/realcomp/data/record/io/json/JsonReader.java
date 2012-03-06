44326f136baf86df73371a5cc6767643227e5bf2
Kyle Renfro <krenfro@real-comp.com>
2012-03-06 21:23:34 +0000

    initial import
    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/realcomp-data-json/trunk@4033 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

---
+++ src/main/java/com/realcomp/data/record/io/json/JsonReader.java
@@ -0,0 +1,234 @@
+package com.realcomp.data.record.io.json;
+
+import com.realcomp.data.Operation;
+import com.realcomp.data.conversion.ConversionException;
+import com.realcomp.data.record.Record;
+import com.realcomp.data.record.io.BaseRecordReaderWriter;
+import com.realcomp.data.record.io.IOContext;
+import com.realcomp.data.record.io.RecordReader;
+import com.realcomp.data.schema.Field;
+import com.realcomp.data.schema.Schema;
+import com.realcomp.data.schema.SchemaException;
+import com.realcomp.data.transform.TransformContext;
+import com.realcomp.data.transform.ValueSurgeon;
+import com.realcomp.data.validation.ValidationException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
+/**
+ * 
+ * The JSON format is rich enough that a Schema is <i>not</i> required to parse a Record.
+ * If a schema <i>is</i> specified, only the fields specified in the schema will appear in the Record.
+ * 
+ * 
+ * @author krenfro
+ */
+public class JsonReader extends BaseRecordReaderWriter implements RecordReader {
+    
+    protected JsonFactory jsonFactory;
+    protected JsonParser jsonParser;
+    protected ValueSurgeon surgeon;
+    protected TransformContext transformContext;
+
+    public JsonReader() {
+        super();
+        format.putDefault("type", "JSON");
+        format.putDefault("pretty", "false");
+        jsonFactory = new JsonFactory();
+        surgeon = new ValueSurgeon();
+        transformContext = new TransformContext();
+    }
+
+    @Override
+    public Record read() throws IOException, ValidationException, ConversionException, SchemaException {
+
+        if (!beforeFirstOperationsRun) {
+            executeBeforeFirstOperations();
+            beforeFirstOperationsRun = true;
+        }
+
+        Record record = null;
+        moveToNextObject();
+        Map map = parseMap();
+
+        if (map != null) {
+            if (context.getSchema() == null){
+                record = new Record(map);
+            }
+            else{
+                /* Since a schema is defined, only put the fields defined in the schema into the final record.
+                 * The operations should be able to find values in the more complete Record parsed from 
+                 * the raw json.  Create a temporary Record from the parsed json, and use that for the
+                 * field creation.  
+                 */
+                record = new Record();
+                Record temp = new Record(map);
+                transformContext.setRecord(temp);
+                for (Field field : context.getSchema().classify(temp)) {
+                    transformContext.setKey(field.getName());
+                    Object value = surgeon.operate(getOperations(field), transformContext);                    
+                    if (value != null){
+                        //Write the results of the operations to both the final Record, and the
+                        // temporary Record for subsequent field creation.
+                        record.put(field.getName(), field.getType().coerce(value));
+                        temp.put(field.getName(), field.getType().coerce(value));
+                    }
+                }
+            }
+            count++;
+        }
+        else {
+            executeAfterLastOperations();
+        }
+
+        return record;
+    }
+
+    private List<Operation> getOperations(Field field) {
+
+        assert(field != null);
+        assert(context.getSchema() != null);
+
+        Schema schema = context.getSchema();
+        List<Operation> operations = new ArrayList<Operation>();
+        if (schema.getBeforeOperations() != null)
+            operations.addAll(schema.getBeforeOperations());
+        operations.addAll(field.getOperations());
+        if (schema.getAfterOperations() != null)
+            operations.addAll(schema.getAfterOperations());
+        return operations;
+    }
+
+    private void moveToNextObject() throws IOException {
+        JsonToken token = jsonParser.nextToken();
+        if (token == JsonToken.START_ARRAY) {
+            //more than one record in the input stream
+            moveToNextObject();
+        }
+        else if (token == JsonToken.START_OBJECT) {
+            //ready - probably only one json object in the input stream
+        }
+    }
+
+    private Map parseMap() throws IOException {
+
+        Map map = null;
+        if (jsonParser.getCurrentToken() == JsonToken.START_OBJECT) {
+
+            map = new HashMap();
+            while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
+
+                JsonToken token = jsonParser.getCurrentToken();
+
+                if (token == JsonToken.START_ARRAY) {
+                    //nested list
+                    map.put(jsonParser.getCurrentName(), parseList());
+                }
+                else if (token == JsonToken.START_OBJECT) {
+                    //nested map
+                    map.put(jsonParser.getCurrentName(), parseMap());
+                }
+                else if (token == JsonToken.VALUE_TRUE) {
+                    map.put(jsonParser.getCurrentName(), Boolean.TRUE);
+                }
+                else if (token == JsonToken.VALUE_FALSE) {
+                    map.put(jsonParser.getCurrentName(), Boolean.FALSE);
+                }
+                else if (token == JsonToken.VALUE_STRING) {
+                    //TODO: charset being used!
+                    map.put(jsonParser.getCurrentName(), jsonParser.getText());
+                }
+                else if (token == JsonToken.VALUE_NUMBER_FLOAT) {
+                    //TODO: charset being used!
+                    map.put(jsonParser.getCurrentName(), Float.valueOf(jsonParser.getFloatValue()));
+                }
+                else if (token == JsonToken.VALUE_NUMBER_INT) {
+                    //TODO: charset being used!
+                    map.put(jsonParser.getCurrentName(), Integer.valueOf(jsonParser.getIntValue()));
+                }
+                else if (token == JsonToken.VALUE_NULL) {
+                    //skip
+                }
+            }
+        }
+
+        return map;
+    }
+
+    private List parseList() throws IOException {
+
+        List list = null;
+
+        if (jsonParser.getCurrentToken() == JsonToken.START_ARRAY) {
+
+            list = new ArrayList();
+            while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+
+                JsonToken token = jsonParser.getCurrentToken();
+
+                if (token == JsonToken.START_ARRAY) {
+                    //nested list
+                    list.add(parseList());
+                }
+                else if (token == JsonToken.START_OBJECT) {
+                    //nested map
+                    list.add(parseMap());
+                }
+                else if (token == JsonToken.VALUE_TRUE) {
+                    list.add(Boolean.TRUE);
+                }
+                else if (token == JsonToken.VALUE_FALSE) {
+                    list.add(Boolean.FALSE);
+                }
+                else if (token == JsonToken.VALUE_NULL) {
+                    //skip
+                }
+                else if (token == JsonToken.VALUE_STRING) {
+                    //TODO: charset being used!
+                    list.add(jsonParser.getText());
+                }
+                else if (token == JsonToken.VALUE_NUMBER_FLOAT) {
+                    //TODO: charset being used!
+                    list.add(Float.valueOf(jsonParser.getFloatValue()));
+                }
+                else if (token == JsonToken.VALUE_NUMBER_INT) {
+                    //TODO: charset being used!
+                    list.add(Integer.valueOf(jsonParser.getIntValue()));
+                }
+            }
+        }
+
+        return list;
+    }
+
+    @Override
+    public void open(IOContext context) throws IOException, SchemaException {
+
+        super.open(context);
+        if (context.getIn() == null)
+            throw new IllegalArgumentException("Invalid IOContext. No InputStream specified");
+        
+        jsonParser = jsonFactory.createJsonParser(context.getIn());
+    }
+
+    @Override
+    public void close(boolean closeIOContext) {
+
+        super.close(closeIOContext);
+        if (jsonParser != null) {
+            try {
+                jsonParser.close();
+            }
+            catch (IOException ex) {
+                Logger.getLogger(JsonReader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/realcomp/data/record/io/json/JsonWriter.java
@@ -0,0 +1,285 @@
+package com.realcomp.data.record.io.json;
+
+import com.realcomp.data.DataType;
+import com.realcomp.data.Operation;
+import com.realcomp.data.conversion.ConversionException;
+import com.realcomp.data.record.Record;
+import com.realcomp.data.record.io.BaseRecordReaderWriter;
+import com.realcomp.data.record.io.IOContext;
+import com.realcomp.data.record.io.RecordWriter;
+import com.realcomp.data.schema.Field;
+import com.realcomp.data.schema.FieldList;
+import com.realcomp.data.schema.SchemaException;
+import com.realcomp.data.transform.TransformContext;
+import com.realcomp.data.transform.Transformer;
+import com.realcomp.data.transform.ValueSurgeon;
+import com.realcomp.data.validation.ValidationException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.util.DefaultPrettyPrinter;
+
+/**
+ *
+ * @author krenfro
+ */
+public class JsonWriter extends BaseRecordReaderWriter implements RecordWriter{
+
+    private static final Logger logger = Logger.getLogger(JsonWriter.class.getName());
+    
+    protected JsonFactory jsonFactory;
+    protected JsonGenerator json;
+    protected Transformer transformer;
+    protected TransformContext xCtx;
+    protected ValueSurgeon surgeon;    
+    
+    public JsonWriter(){
+    
+        super();
+        format.putDefault("pretty", "false");
+        format.putDefault("type", "JSON");
+        
+        jsonFactory = new JsonFactory();
+        jsonFactory.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+        transformer = new Transformer();
+        xCtx = new TransformContext();
+        surgeon = new ValueSurgeon();
+    }
+    
+    @Override
+    public void write(Record record) throws IOException, ValidationException, ConversionException, SchemaException {
+        
+        if (record == null)
+            throw new IllegalArgumentException("record is null");
+
+        if (!beforeFirstOperationsRun){
+            executeBeforeFirstOperations();
+            beforeFirstOperationsRun = true;
+        }
+
+        if (!isPretty() && count > 0){
+            //add newline to output
+            json.writeRaw("\n");
+        }
+        
+        if (context.getSchema() != null){
+            //modify the record, performing all operations and keeping only the fields defined in the schema
+            FieldList fields = context.getSchema().classify(record);
+            transform(record, fields);
+            filterFields(record, fields);
+        }
+        
+        writeJson(record.asSimpleMap());
+        
+        count++;
+        
+    }
+    
+    protected void transform(Record record, FieldList fields) 
+            throws ConversionException, ValidationException, SchemaException{
+        
+        assert(record != null);
+        assert(context.getSchema() != null);
+        
+        transformer.setBefore(context.getSchema().getBeforeOperations());
+        transformer.setAfter(context.getSchema().getAfterOperations());
+        transformer.setFields(fields);
+        xCtx.setRecord(record);
+        transformer.transform(xCtx);
+    }
+    
+    protected void filterFields(Record record, FieldList fields){
+        
+        Set<String> filter = new HashSet<String>();
+        Set<String> keep = new HashSet<String>();
+        for (Field field: fields)
+            keep.add(field.getName());
+        
+        filter.addAll(record.keySet());
+        filter.removeAll(keep);
+        
+        for (String f: filter){
+            record.remove(f);
+        }
+    }
+
+    
+    private void writeJson(Map<String,Object> map)
+            throws ValidationException, ConversionException, IOException{
+
+        json.writeStartObject();
+        
+        for (Map.Entry<String,Object> entry: map.entrySet()){
+            writeJson(entry.getKey(), entry.getValue());
+        }
+        
+        json.writeEndObject();
+    }
+    
+    /**
+     * 
+     * @param map
+     * @return true if the map is not null and contains a list or a map.
+     */
+    private boolean isDeepMap(Map<String,Object> map){
+        
+        boolean deep = true;
+        if (map != null) {           
+            for (Object entry : (List) map) {
+                if (entry != null) {
+                    DataType entryType = DataType.getDataType(entry);
+                    if (entryType == DataType.LIST || entryType == DataType.MAP) {
+                        deep = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return deep;
+    }
+    
+    private void writeJson(String name, Object value) 
+            throws IOException, ValidationException, ConversionException{
+        
+        
+        if (value == null){
+            json.writeFieldName(name);
+            json.writeNull();
+        }
+        else{
+            
+            DataType type = DataType.getDataType(value);            
+            switch(type){
+                case MAP:
+                    json.writeFieldName(name);
+                    writeJson((Map<String,Object>) value);
+                    break;
+                case LIST:
+                    json.writeArrayFieldStart(name);
+                    writeJson(value, type);
+                    json.writeEndArray();
+                    break;
+                default:
+                    json.writeFieldName(name);
+                    writeJson(value, type);
+            }
+        }
+    }
+
+    
+    private void writeJson(Object value, DataType type) 
+            throws IOException, ValidationException, ConversionException{
+        
+        assert(value != null);
+        assert(type != null);
+        
+        switch(type){
+            case STRING:
+                json.writeString((String) value);
+                break;
+            case INTEGER:
+                json.writeNumber((Integer) value);
+                break;
+            case LONG:
+                json.writeNumber((Long) value);
+                break;
+            case FLOAT:
+                json.writeNumber((Float) value);
+                break;
+            case DOUBLE:
+                json.writeNumber((Double) value);
+                break;
+            case BOOLEAN:
+                json.writeBoolean((Boolean) value);
+                break;
+            case MAP:
+                writeJson((Map<String,Object>) value);
+                break;
+            case LIST:
+                for (Object entry: (List) value)
+                    writeJson(entry, DataType.getDataType(entry));
+                break;
+        }
+
+    }
+    
+    
+    @Override
+    public void close(boolean closeIOContext) {
+        
+        try {
+            executeAfterLastOperations();
+        }
+        catch (ValidationException ex) {
+            logger.log(Level.WARNING, null, ex);
+        }
+        catch (ConversionException ex) {
+            logger.log(Level.WARNING, null, ex);
+        }
+        
+        if (json != null){
+            try {
+                json.close();
+            }
+            catch (IOException ex) {
+                logger.log(Level.WARNING, null, ex);
+            }
+        }
+        
+        super.close(closeIOContext);
+    }
+
+    @Override
+    public void open(IOContext context) throws IOException, SchemaException {
+        
+        super.open(context);
+        if (context.getOut() == null)
+            throw new IllegalArgumentException("Invalid IOContext. No OutputStream specified");
+        
+        json = jsonFactory.createJsonGenerator(context.getOut(), JsonEncoding.UTF8);
+        if (isPretty()){
+            json.setPrettyPrinter(new DefaultPrettyPrinter());
+        }
+    }
+
+    
+    @Override
+    protected void executeAfterLastOperations() throws ValidationException, ConversionException{
+        
+        if (context != null && context.getSchema() != null){
+            List<Operation> operations = context.getSchema().getAfterLastOperations();
+            if (operations != null && !operations.isEmpty()){
+                xCtx.setRecordCount(this.getCount());
+                surgeon.operate(operations, xCtx);
+            }
+        }
+
+            
+    }
+
+    @Override
+    protected void executeBeforeFirstOperations() throws ValidationException, ConversionException{
+
+         if (context != null &&context.getSchema() != null){
+            List<Operation> operations = context.getSchema().getBeforeFirstOperations();            
+            if (operations != null && !operations.isEmpty()){
+                xCtx.setRecordCount(this.getCount());
+                surgeon.operate(operations, xCtx);
+            }
+        }
+    }
+
+    
+    public boolean isPretty(){
+        return Boolean.parseBoolean(format.get("pretty"));
+    }
+    
+    
+}
